0dde8ce3018cd7a192155c9cb6982e2a48a8486f
Ryan Bryle <RDBryle@gmail.com>
2017-10-19 16:52:48 -0400

    Update SupportCode.swift

--- PhysicsTests/SupportCode.swift
+++ PhysicsTests/SupportCode.swift
@@ -9,10 +9,6 @@
 import Foundation
 import UIKit
 
-//public func random(min: CGFloat, max: CGFloat) -> CGFloat {
-//  return CGFloat(Float(arc4random()) / Float(0xFFFFFFFF)) * (max - min) + max
-//}
-
 public func random(min: CGFloat, max: CGFloat) -> CGFloat {
   return CGFloat(Float(arc4random()) / Float(0xFFFFFFFF))
     * (max - min) + min
